a008331cd0deb6205399e8dece9a64591f47566c
Andrew Klaassen <klaassenandrew@gmail.com>
2018-03-31 06:20:08 -0500

    Removed Tests and formatted

--- socailmediaanalyzer/src/main/java/com/socailmediaanalyzer/Configuration/TwitterConfig.java
+++ socailmediaanalyzer/src/main/java/com/socailmediaanalyzer/Configuration/TwitterConfig.java
@@ -11,16 +11,16 @@ import org.springframework.social.twitter.api.impl.TwitterTemplate;
 public class TwitterConfig {
 
     @Value("${consumerKey}")
-    private String consumerKey;// = "GFlUcD0WCcggqoorazU58bW1o";
+    private String consumerKey;
 
     @Value("${consumerSecret}")
-    private String consumerSecret;// = "jGt5XfzMCOioLrmoOd7pUZGSK1OEgSCXhG6qsYnyfRwpcoz1Gi";
+    private String consumerSecret;
 
     @Value("${accessToken}")
-    private String accessToken;// = "971456480326144000-p2BempWJerVwO1AtkmY1nlL2TNYIPyy";
+    private String accessToken;
 
     @Value("${accessTokenSecret}")
-    private String accessTokenSecret;// = "fbcVlavyow0jbiEPsI619KxT1rrZgqr3ntHElzp6C5esK";
+    private String accessTokenSecret;
 
     public Twitter getTwitterTemplate() {
         return new TwitterTemplate(consumerKey, consumerSecret, accessToken, accessTokenSecret);
